62a717691b7b06a550a837faa926635599627d02
A-yum3 <noikari73@gmail.com>
2022-01-28 22:02:14 +0900

    ファイルを適切なサイズに分割

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+8
+Procfile,b/c/bc3c68e880d1a01632bce79f69dd1387c7ba9c54
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- main.py
+++ main.py
@@ -1,60 +1,11 @@
 import os
 
-from slack_bolt import App
-from slack_sdk import WebClient
-
-from converter import Converter
-
-client = WebClient(os.environ["SLACK_BOT_TOKEN"])
-
-app = App(
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
-    token=os.environ.get("SLACK_BOT_TOKEN")
-)
-
-
-# 絵文字が付けられたら付けられたスレッドを読む
-@app.event('reaction_added')
-def reaction_add(event, say):
-    # response処理 TODO toFunction
-    emoji = event["reaction"]
-    if emoji != "pdf":
-        return
-    channel = event["item"]["channel"]
-    ts = event["item"]["ts"]
-    item_user = event["item_user"]
-
-    # timestampからメッセージ特定
-    group_history = client.conversations_replies(channel=channel, ts=ts)
-    messages = group_history.data["messages"]
-
-    # スレッド元でない可能性を考慮し、再取得 TODO refactor
-    ts = messages[0]["thread_ts"]
-    group_history = client.conversations_replies(channel=channel, ts=ts)
-    messages = group_history.data["messages"]
-    max_message_count = int(messages[0]["reply_count"])
-    title = messages[0]["text"]
-
-    output_messages = []
-    for i in range(1, max_message_count + 1):  # 1-origin
-        output_messages.append(messages[i]["text"])
-
-    file_name = Converter(output_messages).to_pdf()
-
-    client.files_upload(
-        channels=channel,
-        initial_comment="スレッドで要件定義はやめてくれ委員会会長",
-        file=file_name
-    )
-
-
 from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+from app import app
 
 flask_app = Flask(__name__)
-
-# SlackRequestHandler は WSGI のリクエストを Bolt のインターフェイスに合った形に変換します
-# Bolt レスポンスからの WSGI レスポンスの作成も行います
-from slack_bolt.adapter.flask import SlackRequestHandler
 
 handler = SlackRequestHandler(app)
 
@@ -62,7 +13,6 @@ handler = SlackRequestHandler(app)
 # Flask アプリへのルートを登録します
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
-    # handler はアプリのディスパッチメソッドを実行します
     return handler.handle(request)
 
 
