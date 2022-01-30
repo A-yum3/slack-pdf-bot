7762856817160f3d080128f4db377fc5bf7cbef5
A-yum3 <noikari73@gmail.com>
2022-01-31 00:17:57 +0900

    feat(WIP): PDFConverterのSample実装

--- converter.py
+++ converter.py
@@ -1,11 +1,15 @@
-from reportlab.lib.pagesizes import A4, portrait
+from reportlab.lib.pagesizes import mm, portrait, A4
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import cidfonts
 from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfgen import canvas
+from reportlab.platypus import BaseDocTemplate, PageTemplate
+from reportlab.platypus import Paragraph, PageBreak
+from reportlab.platypus.flowables import Spacer
+from reportlab.platypus.frames import Frame
 
 
 class Converter:
-    # TODO Messageを受け取るように適合
+    # TODO Messageを受け取るように修正
     def __init__(self, value_lists):
         self.value_lists = value_lists
 
@@ -14,15 +18,52 @@ class Converter:
         pdfmetrics.registerFont(cidfonts.UnicodeCIDFont("HeiseiMin-W3"))
 
         file_name = 'output.pdf'  # ファイル名を設定
-        pdf = canvas.Canvas(file_name, pagesize=portrait(A4))
-        width, height = A4
-        pdf.setFont('HeiseiMin-W3', 16)
 
-        # insert text
-        for index, text in enumerate(self.value_lists):
-            pdf.drawString(x=50, y=(height - 50) - (index * 50), text=text)
+        doc = BaseDocTemplate(file_name, title="test", pagesize=portrait(A4))
 
-        pdf.save()
+        show = 1  # Frameの枠を表示
+        frames = [
+            Frame(10 * mm, 20 * mm, 190 * mm, 260 * mm, showBoundary=show),
+        ]
+
+        page_template = PageTemplate("frames", frames=frames)
+        doc.addPageTemplates(page_template)
+        style_dict = {
+            "name": "nomarl",
+            "fontName": "HeiseiMin-W3",
+            "fontSize": 20,
+            "leading": 20,
+            "firstLineIndent": 20,
+        }
+        style = ParagraphStyle(**style_dict)
+
+        flowables = []
+
+        space = Spacer(10 * mm, 10 * mm)
+
+        para = Paragraph("こんなものは、reportlabではない!!<br/><br/>", style)
+        flowables.append(para)
+        flowables.append(space)
+        para = Paragraph("本当のreportlabをお見せします。", style)
+        flowables.append(para)
+        flowables.append(space)
+        para = Paragraph("次のページに来てください", style)
+        flowables.append(para)
+
+        # 改ページ
+        flowables.append(PageBreak())
+
+        para = Paragraph("<span backcolor=yellow>フーレム1</span>", style)
+        flowables.append(para)
+        para = Paragraph("Paragraphを使うことで長い文章を自動で折り返してくれます。さらにはみ出せば次のページに進みます。", style)
+        flowables.append(para)
+        para = Paragraph("ちゃんと続きの高さから次のParagraphを描画してくれます。", style)
+        flowables.append(para)
+        para = Paragraph("Frameを使えば、はみ出した分は次のフレームまたはページへ自動で進みます。", style)
+        flowables.append(para)
+        para = Paragraph("Frameを使うことで、段組みのような複雑なレイアウトを表現できます。", style)
+        flowables.append(para)
+
+        doc.multiBuild(flowables)
+
         return file_name
-
-# Converter(['こんにちは', '世界']).to_pdf()
